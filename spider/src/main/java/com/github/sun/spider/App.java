f287bb3df4936fd9c09f83d5663837f6c98d855b
蔺绍宏 <shaohonglin0411@gmail.com>
2019-07-11 21:53:53 +0800

    modify

--- spider/src/main/java/com/github/sun/spider/App.java
+++ spider/src/main/java/com/github/sun/spider/App.java
@@ -1,5 +1,6 @@
 package com.github.sun.spider;
 
+import com.github.sun.foundation.boot.utility.JSON;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.github.sun"})
 public class App {
   public static void main(String[] args) {
+    JSON.asJsonNode("");
     new SpringApplicationBuilder(App.class).run(args);
   }
 }
--- spider/src/main/java/com/github/sun/spider/rest/SpiderResource.java
+++ spider/src/main/java/com/github/sun/spider/rest/SpiderResource.java
@@ -1,5 +1,6 @@
 package com.github.sun.spider.rest;
 
+
 import com.github.sun.foundation.rest.AbstractResource;
 
 import javax.ws.rs.Consumes;
