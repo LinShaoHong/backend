beb5a784eb6d04d2f613fdcf5e89237ee39e3832
LinShaoHong <lins@LinShaoHong-MacBook.local>
2020-06-15 11:04:25 +0800

    fix

--- qm/rest/src/main/java/com/github/sun/qm/CollectionResource.java
+++ qm/rest/src/main/java/com/github/sun/qm/CollectionResource.java
@@ -27,105 +27,104 @@ import java.util.stream.Collectors;
 @Produces(MediaType.APPLICATION_JSON)
 @Api(value = "Collection Resource")
 public class CollectionResource extends AbstractResource {
-  private final CollectionMapper mapper;
-  private final CollectionService service;
-  private final GirlMapper girlMapper;
-  private final SqlBuilder.Factory factory;
+    private final CollectionMapper mapper;
+    private final CollectionService service;
+    private final GirlMapper girlMapper;
+    private final SqlBuilder.Factory factory;
 
-  @Inject
-  public CollectionResource(CollectionMapper mapper,
-                            GirlMapper girlMapper,
-                            UserMapper userMapper,
-                            CollectionService service,
-                            @Named("mysql") SqlBuilder.Factory factory) {
-    this.mapper = mapper;
-    this.girlMapper = girlMapper;
-    this.service = service;
-    this.factory = factory;
-  }
-
-  @GET
-  @ApiOperation("获取我的收藏")
-  public PageResponse<CollectionRes> list(@QueryParam("start") int start,
-                                          @QueryParam("count") int count,
-                                          @Context User user) {
-    SqlBuilder sb = factory.create();
-    Expression condition = sb.field("userId").eq(user.getId());
-    int total = mapper.countByTemplate(sb.from(Collection.class).where(condition).count().template());
-    if (start < total) {
-      sb.clear();
-      SqlBuilder.Template template = sb.from(Collection.class)
-        .where(condition)
-        .desc("createTime")
-        .limit(start, count)
-        .template();
-      List<Collection> list = mapper.findByTemplate(template);
-      Set<String> girlIds = list.stream().map(Collection::getGirlId).collect(Collectors.toSet());
-      Map<String, Girl> girls = girlMapper.findByIds(girlIds).stream().collect(Collectors.toMap(Girl::getId, v -> v));
-      return responseOf(total, list.stream().map(v -> {
-        Girl g = girls.get(v.getGirlId());
-        if (g != null) {
-          return CollectionRes.builder()
-            .id(v.getId())
-            .girlId(g.getId())
-            .name(g.getName())
-            .city(g.getCity())
-            .type(g.getType())
-            .mainImage(g.getMainImage())
-            .onService(g.isOnService())
-            .time(Dates.simpleTime(v.getUpdateTime()))
-            .build();
-        }
-        return null;
-      }).filter(Objects::nonNull).collect(Collectors.toList()));
+    @Inject
+    public CollectionResource(CollectionMapper mapper,
+                              GirlMapper girlMapper,
+                              CollectionService service,
+                              @Named("mysql") SqlBuilder.Factory factory) {
+        this.mapper = mapper;
+        this.girlMapper = girlMapper;
+        this.service = service;
+        this.factory = factory;
     }
-    return responseOf(total, Collections.emptyList());
-  }
 
-  @Data
-  @Builder
-  @NoArgsConstructor
-  @AllArgsConstructor
-  private static class CollectionRes {
-    private String id;
-    private String girlId;
-    private String name;
-    private String city;
-    private Girl.Type type;
-    private String mainImage;
-    private String time;
-    private boolean onService;
-  }
+    @GET
+    @ApiOperation("获取我的收藏")
+    public PageResponse<CollectionRes> list(@QueryParam("start") int start,
+                                            @QueryParam("count") int count,
+                                            @Context User user) {
+        SqlBuilder sb = factory.create();
+        Expression condition = sb.field("userId").eq(user.getId());
+        int total = mapper.countByTemplate(sb.from(Collection.class).where(condition).count().template());
+        if (start < total) {
+            sb.clear();
+            SqlBuilder.Template template = sb.from(Collection.class)
+                    .where(condition)
+                    .desc("createTime")
+                    .limit(start, count)
+                    .template();
+            List<Collection> list = mapper.findByTemplate(template);
+            Set<String> girlIds = list.stream().map(Collection::getGirlId).collect(Collectors.toSet());
+            Map<String, Girl> girls = girlMapper.findByIds(girlIds).stream().collect(Collectors.toMap(Girl::getId, v -> v));
+            return responseOf(total, list.stream().map(v -> {
+                Girl g = girls.get(v.getGirlId());
+                if (g != null) {
+                    return CollectionRes.builder()
+                            .id(v.getId())
+                            .girlId(g.getId())
+                            .name(g.getName())
+                            .city(g.getCity())
+                            .type(g.getType())
+                            .mainImage(g.getMainImage())
+                            .onService(g.isOnService())
+                            .time(Dates.simpleTime(v.getUpdateTime()))
+                            .build();
+                }
+                return null;
+            }).filter(Objects::nonNull).collect(Collectors.toList()));
+        }
+        return responseOf(total, Collections.emptyList());
+    }
 
-  @POST
-  @ApiOperation("添加")
-  public Response add(@Valid @NotNull(message = "require body") CollectionReq req,
-                      @Context User user) {
-    service.add(user.getId(), req.getGirlId());
-    return responseOf();
-  }
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class CollectionRes {
+        private String id;
+        private String girlId;
+        private String name;
+        private String city;
+        private Girl.Type type;
+        private String mainImage;
+        private String time;
+        private boolean onService;
+    }
 
-  @Data
-  private static class CollectionReq {
-    @NotEmpty(message = "缺少教师")
-    private String girlId;
-  }
+    @POST
+    @ApiOperation("添加")
+    public Response add(@Valid @NotNull(message = "require body") CollectionReq req,
+                        @Context User user) {
+        service.add(user.getId(), req.getGirlId());
+        return responseOf();
+    }
 
-  @DELETE
-  @Path("/byGirlId")
-  @ApiOperation("添加")
-  public Response delete(@Valid @NotNull(message = "require body") CollectionReq req,
-                         @Context User user) {
-    mapper.deleteByUserIdAndGirlId(user.getId(), req.getGirlId());
-    return responseOf();
-  }
+    @Data
+    private static class CollectionReq {
+        @NotEmpty(message = "缺少教师")
+        private String girlId;
+    }
 
-  @DELETE
-  @Path("/${id}")
-  @ApiOperation("删除")
-  public Response delete(@PathParam("id") String id,
-                         @Context User user) {
-    mapper.deleteById(id);
-    return responseOf();
-  }
+    @DELETE
+    @Path("/byGirlId")
+    @ApiOperation("添加")
+    public Response delete(@Valid @NotNull(message = "require body") CollectionReq req,
+                           @Context User user) {
+        mapper.deleteByUserIdAndGirlId(user.getId(), req.getGirlId());
+        return responseOf();
+    }
+
+    @DELETE
+    @Path("/${id}")
+    @ApiOperation("删除")
+    public Response delete(@PathParam("id") String id,
+                           @Context User user) {
+        mapper.deleteById(id);
+        return responseOf();
+    }
 }
