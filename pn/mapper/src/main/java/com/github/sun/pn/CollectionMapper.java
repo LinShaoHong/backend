a4fceb11131e7b80bed1a79f947fa70bfa25d767
LinShaoHong <shaohonglin0411@gmail.com>
2022-05-31 09:54:31 +0800

    'fix'

--- pn/mapper/src/main/java/com/github/sun/pn/CollectionMapper.java
+++
@@ -1,8 +0,0 @@
-package com.github.sun.pn;
-
-import com.github.sun.foundation.mybatis.CompositeMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CollectionMapper extends CompositeMapper<Collection> {
-}
