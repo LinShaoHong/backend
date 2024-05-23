73e20054c05f6088984d1b0fe72563f92eceb371
LinShaoHong <shaohonglin0411@gmail.com>
2024-05-23 15:57:14 +0800

    fx

---
+++ card/rest/src/main/java/com/github/sun/card/CardAiResource.java
@@ -0,0 +1,36 @@
+package com.github.sun.card;
+
+import com.github.sun.foundation.rest.AbstractResource;
+import lombok.Data;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/ai")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CardAiResource extends AbstractResource {
+  private final CardAiService service;
+
+  @Inject
+  public CardAiResource(CardAiService service) {
+    this.service = service;
+  }
+
+
+  @POST
+  @Path("/chat")
+  public ListResponse<String> chat(@Valid AiReq q) {
+    return responseOf(service.chat(q.getContent()));
+  }
+
+  @Data
+  public static class AiReq {
+    private String content;
+  }
+}
---
+++ card/service/src/main/java/com/github/sun/card/CardAiService.java
@@ -0,0 +1,25 @@
+package com.github.sun.card;
+
+import com.github.sun.foundation.ai.Assistant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Named;
+import java.util.List;
+
+@Service
+@RefreshScope
+@RequiredArgsConstructor
+public class CardAiService {
+  @Value("${qwen.key}")
+  private String apiKey;
+  @Value("${qwen.model}")
+  private String model;
+  private final @Named("qwen") Assistant assistant;
+
+  public List<String> chat(String content) {
+    return assistant.chat(apiKey, model, content);
+  }
+}
