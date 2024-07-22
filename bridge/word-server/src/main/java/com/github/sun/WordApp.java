ec74150cda650e1d7967e74b64dd779ccacf34be
LinShaoHong <shaohonglin0411@gmail.com>
2024-07-22 14:30:30 +0800

    f

---
+++ bridge/word-server/src/main/java/com/github/sun/WordApp.java
@@ -0,0 +1,20 @@
+package com.github.sun;
+
+import com.github.sun.foundation.rest.JerseyApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class WordApp extends JerseyApplication {
+  public WordApp(ApplicationContext context) {
+    super(context);
+  }
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(WordApp.class).run(args);
+  }
+}
