491aa80fa5c604788d31c8ea64d4957667ad9e3c
LinShaoHong <shaohonglin0411@gmail.com>
2024-09-19 16:26:17 +0800

    f

--- word/service/src/main/java/com/github/sun/word/spider/WordXxEnSpider.java
+++ word/service/src/main/java/com/github/sun/word/spider/WordXxEnSpider.java
@@ -16,18 +16,18 @@ public class WordXxEnSpider {
     public static void fetchDerivative(String word, Consumer<Set<String>> func) {
         try {
             Set<String> words = new LinkedHashSet<>();
-            Document node = WordDictLoader.fetchDocument("https://www.xxenglish.com/w1/" + word);
-            List<Node> arr = XPaths.of(node, "//article/p").asArray();
-            int j = -1;
-            for (int i = 0; i < arr.size(); i++) {
-                if (arr.get(i).getTextContent().contains("词形变化：")) {
-                    j = i + 1;
-                    break;
-                }
-            }
-            if (j > 0) {
-                XPaths.of(arr.get(j), "./a").asArray().forEach(a -> words.add(a.getTextContent()));
-            }
+//            Document node = WordDictLoader.fetchDocument("https://www.xxenglish.com/w1/" + word);
+//            List<Node> arr = XPaths.of(node, "//article/p").asArray();
+//            int j = -1;
+//            for (int i = 0; i < arr.size(); i++) {
+//                if (arr.get(i).getTextContent().contains("词形变化：")) {
+//                    j = i + 1;
+//                    break;
+//                }
+//            }
+//            if (j > 0) {
+//                XPaths.of(arr.get(j), "./a").asArray().forEach(a -> words.add(a.getTextContent()));
+//            }
             func.accept(words);
         } catch (Throwable ex) {
             //do nothing
